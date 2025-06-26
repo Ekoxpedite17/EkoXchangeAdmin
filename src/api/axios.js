9ff5d3e7ae6b389593a29ffbc368c9436fad2bfa
Senninseyi <seyi.oyebamiji@gmail.com>
2025-06-26 12:51:25 +0100

    refactor: update axios instance configuration and clean up unused code

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,49 +1,33 @@
-import axios from 'axios';
-import { saveUserData } from '../redux/reducers/auth.reducer';
-import store from '../redux/store';
+import axios from "axios";
+import { saveUserData } from "../redux/reducers/auth.reducer";
+import store from "../redux/store";
 
-const baseURL = 'https://ekoxpedite.shop/api/v1/';
-// const baseURL = 'http://localhost:4000/api/v1/';
-// const baseURL = 'http://192.168.114.15:4000/api/v1/';
+// const baseURL = 'https://api.ekoxchange.com/api/v1/';
+const baseURL = "http://localhost:4000/api/v1/";
+// const baseURL = "http://172.20.10.2:4000/api/v1/";
 
 const axiosInstance = axios.create({
   baseURL,
   headers: {
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json",
   },
-  withCredentials: true
 });
-
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const userdata = JSON.parse(localStorage.getItem('persist:auth'));
-    const token = userdata && JSON.parse(userdata.user);
-    if (token) {
-      config.headers.cookie = `eko-admin-gateway=${token.id}`;
-      axios.defaults.withCredentials = true;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 axiosInstance.interceptors.response.use(
   (response) => {
-    console.log('Response received:', response);
+    console.log("Response received:", response);
 
     return response;
   },
   (error) => {
     if (error.response && error.response.status === 401) {
-      console.warn('Unauthorized. Redirecting to login...');
-      store.dispatch(
-        saveUserData({
-          user: null,
-          isAuthenticated: false
-        })
-      );
+      console.warn("Unauthorized. Redirecting to login...");
+      // store.dispatch(
+      //   saveUserData({
+      //     user: null,
+      //     isAuthenticated: false
+      //   })
+      // );
     }
     return Promise.reject(error);
   }
